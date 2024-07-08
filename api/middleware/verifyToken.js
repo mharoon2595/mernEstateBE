32156ef1da736204419f849690e4c5067492d4a4
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-08 09:25:11 +0530

    .

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
 
+  console.log("token--->", token);
+
   jwt.verify(token, process.env.SECRET_KEY, async (error, payload) => {
     if (error) return res.status(401).json({ message: "Not authorized" });
     req.userId = payload.id;
