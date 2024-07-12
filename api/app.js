77250b0c38dfe8668fd52e3fb84126e4d47ec3db
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-07-12 08:27:59 +0530

    temp changes again

--- api/app.js
+++ api/app.js
@@ -9,8 +9,23 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
+const allowedOrigins = [
+  "https://mernestate.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
